9937dbd9403705f11c54b64c9aa3aa2645d42ced
Bali Miklós <miklos.bali@dbx.hu>
2021-12-03 09:30:08 +0100

    Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,16 +1,68 @@
 fun main() {
 
+    fun toMeasurements(input: List<String>) = input
+        .map { it.toCharArray().map { digit -> digit.digitToInt(2) } }
+
+    fun List<Int>.sortedCountsByValues(): List<Pair<Int, Int>> = groupingBy { it }
+        .eachCount()
+        .toList()
+        .sortedBy { it.second }
+
+    fun List<Int>.binaryDigitsToInt(): Int = joinToString("").toInt(2)
+
+    fun mostCommonBit(measurements: List<List<Int>>, position: Int): Int {
+        val (bit, count) = measurements
+            .map { it[position] }
+            .sortedCountsByValues()
+            .last()
+        return if (count * 2 == measurements.size)
+            1 //if the counts are exactly the same then return 1
+        else
+            bit
+    }
+
+    fun leastCommonBit(measurements: List<List<Int>>, position: Int): Int {
+        //cannot be 1-mostCommonBit (in case all bits are the same)
+        val (bit, count) = measurements
+            .map { it[position] }
+            .sortedCountsByValues()
+            .first()
+        return if (count * 2 == measurements.size)
+            0 //if the counts are exactly the same then return 0
+        else
+            bit
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val measurements = toMeasurements(input)
+        val bitLength = measurements.first().size
+        val gamma = List(bitLength) { idx -> mostCommonBit(measurements, idx) }.binaryDigitsToInt()
+        val epsilon = List(bitLength) { idx -> leastCommonBit(measurements, idx) }.binaryDigitsToInt()
+
+        return gamma * epsilon
+    }
+
+    fun reduceCandidates(measurements: List<List<Int>>, bitProvider: (List<List<Int>>, Int) -> Int): Int {
+        val candidates = measurements.toMutableList()
+        var position = 0
+        while (position < measurements.first().size && candidates.size > 0) {
+            val filterBit = bitProvider(candidates, position)
+            candidates.removeAll { it[position] != filterBit }
+            position++
+        }
+        return candidates.first().binaryDigitsToInt()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val measurements = toMeasurements(input)
+        val o2GeneratorRating = reduceCandidates(measurements, ::mostCommonBit)
+        val co2ScrubberRating = reduceCandidates(measurements, ::leastCommonBit)
+        return o2GeneratorRating * co2ScrubberRating
     }
 
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 0)
-//    check(part2(testInput) == 0)
+    check(part1(testInput) == 198)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
