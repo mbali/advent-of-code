ca1974381ba36cf21b697b06419d596b15a8093b
Bali Miklós <miklos.bali@dbx.hu>
2021-12-06 10:08:55 +0100

    Day 6 - part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,7 +1,7 @@
 fun main() {
-    fun tick(input: IntArray): IntArray {
+    fun tick(input: LongArray): LongArray {
         check(input.size == 9) { "Input size should be 9" }
-        val result = IntArray(9)
+        val result = LongArray(9)
         for (i in 1..8) {
             result[i - 1] = input[i]
         }
@@ -10,25 +10,29 @@ fun main() {
         return result
     }
 
-    fun part1(input: List<String>): Int {
+    fun solution(input: List<String>, days: Int): Long {
         val initialTimers = input.first().split(',').map { it.toInt() }
-        var countsByTimer = IntArray(9) { idx -> initialTimers.count { it == idx } }
-        for (days in 1..80) {
+        var countsByTimer = LongArray(9) { idx -> initialTimers.count { it == idx }.toLong() }
+        repeat(days) {
             countsByTimer = tick(countsByTimer)
         }
         return countsByTimer.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>): Long {
+        return solution(input, 80)
+    }
+
+    fun part2(input: List<String>): Long {
+        return solution(input, 256)
     }
 
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 5934)
+    check(part1(testInput) == 5_934L)
 
     val input = readInput("Day06")
     println(part1(input))
 
-    check(part2(testInput) == TODO())
+    check(part2(testInput) == 26_984_457_539L)
     println(part2(input))
 }
