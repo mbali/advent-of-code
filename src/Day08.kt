19d3034ad4fd5f7a6be403bfd2a60044a59cb890
Bali Miklós <miklos.bali@dbx.hu>
2021-12-08 07:48:17 +0100

    Day 8 - part 1

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,15 +1,31 @@
+private data class PuzzleEntry(val patterns: List<String>, val output: List<String>)
+
+private fun String.toBits() =
+    ('a'..'g').joinToString(separator = "") { if (this.contains(it)) "1" else "0" }
+
+
+private fun String.toPuzzleEntry(): PuzzleEntry {
+    val patterns = Regex("""[a-g]+""").findAll(this).map { it.value.toBits() }.toList()
+    check(patterns.size == 14) { "Single entry should contain 14 patterns" }
+    return PuzzleEntry(patterns.take(10), patterns.takeLast(4))
+}
+
+private fun String.popCnt() = this.count { it == '1' }
+
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        TODO()
-    }
+    fun part1(input: List<String>): Int =
+        input.map { it.toPuzzleEntry() }
+            .flatMap { entry -> entry.output.map { it.popCnt() } }
+            .count { setOf(2, 3, 4, 7).contains(it) }
+
 
     fun part2(input: List<String>): Int {
         TODO()
     }
 
     val testInput = readInput("Day08_test")
-    check(part1(testInput) == TODO())
+    check(part1(testInput) == 26)
 
     val input = readInput("Day08")
     println(part1(input))
