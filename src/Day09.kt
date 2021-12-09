7c57c767906e881b7ca9877d182b802bee9dada2
Bali Miklós <miklos.bali@dbx.hu>
2021-12-09 10:20:20 +0100

    Day 9 - part 2 - brute force

--- src/Day09.kt
+++ src/Day09.kt
@@ -15,6 +15,10 @@ private val neigboursDirections = listOf(
     -1 to 0
 )
 
+private fun Pair<Int, Int>.isNeighbour(other: Pair<Int, Int>): Boolean = neigboursDirections
+    .map { first + it.first to second + it.second }
+    .any { it == other }
+
 fun main() {
 
     fun part1(input: List<String>): Int {
@@ -35,7 +39,22 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val heightMap = input.asHeightMap()
+        val nonFloodedPositions = heightMap.indices.flatMap { row ->
+            heightMap[row].indices.filter { col -> heightMap[row][col] < 9 }.map { row to it }
+        }.toMutableList()
+        val basins = mutableListOf<List<Pair<Int, Int>>>()
+        while (nonFloodedPositions.isNotEmpty()) {
+            val basin = mutableListOf(nonFloodedPositions.removeFirst())
+            do {
+                val connectedPositions =
+                    nonFloodedPositions.filter { candidate -> basin.any { it.isNeighbour(candidate) } }
+                basin.addAll(connectedPositions)
+                nonFloodedPositions.removeAll(connectedPositions)
+            } while (connectedPositions.isNotEmpty())
+            basins.add(basin)
+        }
+        return basins.map { it.size }.sortedDescending().take(3).reduce { a, b -> a * b }
     }
 
     val testInput = readInput("Day09_test")
@@ -44,6 +63,6 @@ fun main() {
     val input = readInput("Day09")
     println(part1(input))
 
-    check(part2(testInput) == TODO())
+    check(part2(testInput) == 1134)
     println(part2(input))
 }
