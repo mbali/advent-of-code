4024036753c5751210705565fb2c193824007b15
Bali Miklós <miklos.bali@dbx.hu>
2021-12-10 06:52:50 +0100

    Day 10 - part 1

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,7 +1,45 @@
 fun main() {
 
+    class Parser constructor(val input: String) {
+        private val closingPairs = mapOf(
+            '(' to ')',
+            '[' to ']',
+            '{' to '}',
+            '<' to '>'
+        )
+
+        val expectedClosers = ArrayDeque<Char>()
+        val firstErrorPosition: Int?
+
+        init {
+            var errorPosition: Int? = null
+            input.toCharArray().forEachIndexed { idx, char ->
+                if (char in closingPairs.keys) {
+                    expectedClosers.addFirst(closingPairs.getValue(char))
+                } else {
+                    if (expectedClosers.removeFirstOrNull() != char) {
+                        errorPosition = idx
+                        return@forEachIndexed
+                    }
+                }
+            }
+            firstErrorPosition = errorPosition
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        TODO()
+        val scores = mapOf(
+            ')' to 3,
+            ']' to 57,
+            '}' to 1197,
+            '>' to 25137
+        )
+
+        return input
+            .map { Parser(it) }
+            .filter { it.firstErrorPosition != null }
+            .map { it.input[it.firstErrorPosition!!] }
+            .sumOf { scores.getOrDefault(it, 0) }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +47,7 @@ fun main() {
     }
 
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == TODO())
+    check(part1(testInput) == 26397)
 
     val input = readInput("Day10")
     println(part1(input))
