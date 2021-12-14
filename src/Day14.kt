30d58f266bc98d30d3f3f832f5a4d60ce754cc36
Bali Miklós <miklos.bali@dbx.hu>
2021-12-14 15:12:38 +0100

    Day 14 - part 2

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,7 +1,40 @@
 fun main() {
+
+    class Polymer(val start: Char, val end: Char, val pairCounts: Map<String, Long>) {
+        fun expand(rules: Map<String, String>): Polymer {
+            val newCounts = pairCounts
+                .flatMap { (pair, count) ->
+                    rules.getOrDefault(pair, pair).windowed(2).map { it to count } //maybe expand pair to 2 new pairs
+                }
+                .groupingBy { it.first }
+                .fold(0L) { acc, element -> acc + element.second }
+            return Polymer(start, end, newCounts)
+        }
+
+        fun score(): Long {
+            val counts = buildMap<Char, Long> {
+                //first and last chars are in only one pair
+                put(start, 1L)
+                put(end, 1L)
+                pairCounts.forEach { (pair, count) ->
+                    pair.forEach {
+                        merge(it, count) { acc, v -> acc + v }
+                    }
+                }
+            }.values.map { it / 2 } //each value is counted twice
+            return counts.maxOf { it } - counts.minOf { it }
+        }
+    }
+
+    fun String.toPolymer(): Polymer {
+        check(length >= 2) { "Template should be at least 2 characters long" }
+        val counts = this.windowed(2).groupingBy { it }.eachCount().mapValues { (_, v) -> v.toLong() }
+        return Polymer(first(), last(), counts)
+    }
+
     fun List<String>.toExpansions(): Map<String, String> {
         val expansionRegex = Regex("""(?<from>..) -> (?<insert>.)""")
-        return this@toExpansions.mapNotNull { line ->
+        return this.mapNotNull { line ->
             expansionRegex.matchEntire(line)?.let { it ->
                 val from = it.groups["from"]!!.value
                 val insert = it.groups["insert"]!!.value.first()
@@ -10,35 +43,28 @@ fun main() {
         }.toMap()
     }
 
-    fun String.expand(rules: Map<String, String>): String =
-        this.windowed(2)
-            .joinToString("") { pair ->
-                rules.getOrDefault(pair, pair).dropLast(1)
-            } + this.last()
-
-    fun score(polymer: String): Int {
-        val counts = polymer.groupingBy { it }.eachCount()
-        return counts.maxOf { it.value } - counts.minOf { it.value }
-    }
-
-    fun part1(input: List<String>): Int {
-        val template = input.first()
+    fun solution(input: List<String>, steps: Int): Long {
+        val template = input.first().toPolymer()
         val rules = input.drop(2).toExpansions()
-        return score(generateSequence(template) {
+        return generateSequence(template) {
             it.expand(rules)
-        }.elementAt(10))
+        }.elementAt(steps).score()
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>): Long {
+        return solution(input, 10)
+    }
+
+    fun part2(input: List<String>): Long {
+        return solution(input, 40)
     }
 
     val testInput = readInput("Day14_test")
-    check(part1(testInput) == 1588)
+    check(part1(testInput) == 1588L)
 
     val input = readInput("Day14")
     println(part1(input))
 
-    check(part2(testInput) == TODO())
+    check(part2(testInput) == 2188189693529L)
     println(part2(input))
 }
