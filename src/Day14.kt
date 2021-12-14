c91dee3fda5be9d29daeba5094f2a6363ee1f31f
Bali Miklós <miklos.bali@dbx.hu>
2021-12-14 07:31:22 +0100

    Day 14 - part 1

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,7 +1,32 @@
 fun main() {
+    fun List<String>.toExpansions(): Map<String, String> {
+        val expansionRegex = Regex("""(?<from>..) -> (?<insert>.)""")
+        return this@toExpansions.mapNotNull { line ->
+            expansionRegex.matchEntire(line)?.let { it ->
+                val from = it.groups["from"]!!.value
+                val insert = it.groups["insert"]!!.value.first()
+                from to listOf(from[0], insert, from[1]).joinToString("")
+            }
+        }.toMap()
+    }
+
+    fun String.expand(rules: Map<String, String>): String =
+        this.windowed(2)
+            .joinToString("") { pair ->
+                rules.getOrDefault(pair, pair).dropLast(1)
+            } + this.last()
+
+    fun score(polymer: String): Int {
+        val counts = polymer.groupingBy { it }.eachCount()
+        return counts.maxOf { it.value } - counts.minOf { it.value }
+    }
 
     fun part1(input: List<String>): Int {
-        TODO()
+        val template = input.first()
+        val rules = input.drop(2).toExpansions()
+        return score(generateSequence(template) {
+            it.expand(rules)
+        }.elementAt(10))
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +34,7 @@ fun main() {
     }
 
     val testInput = readInput("Day14_test")
-    check(part1(testInput) == TODO())
+    check(part1(testInput) == 1588)
 
     val input = readInput("Day14")
     println(part1(input))
