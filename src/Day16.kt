aa86bc415f078e6645281392ef5062740f319084
Bali Miklós <miklos.bali@dbx.hu>
2021-12-16 12:06:47 +0100

    Day 16 - part 2

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,19 +1,73 @@
-sealed class BitPacket(val version: Int, val typeId: Int, val position: Int) {
-    class Literal(version: Int, typeId: Int, position: Int, val valueBits: String) :
-        BitPacket(version, typeId, position)
+sealed class BitPacket(val version: Int) {
+    abstract fun value(): Long
 
-    class Operator(version: Int, typeId: Int, position: Int, val subPackets: List<BitPacket>) :
-        BitPacket(version, typeId, position)
+    class Literal(version: Int, val literal: Long) :
+        BitPacket(version) {
+        override fun value(): Long = literal
+    }
+
+    sealed class Operator(version: Int, val subPackets: List<BitPacket>) :
+        BitPacket(version) {
+        class Sum(version: Int, subPackets: List<BitPacket>) :
+            Operator(version, subPackets) {
+            override fun value(): Long = subPackets.sumOf { it.value() }
+        }
+
+        class Product(version: Int, subPackets: List<BitPacket>) :
+            Operator(version, subPackets) {
+            override fun value(): Long = subPackets.fold(1L) { acc, packet -> acc * packet.value() }
+        }
+
+        class Min(version: Int, subPackets: List<BitPacket>) :
+            Operator(version, subPackets) {
+            override fun value(): Long = subPackets.minOf { it.value() }
+        }
+
+        class Max(version: Int, subPackets: List<BitPacket>) :
+            Operator(version, subPackets) {
+            override fun value(): Long = subPackets.maxOf { it.value() }
+        }
+
+        class GreaterThan(version: Int, subPackets: List<BitPacket>) :
+            Operator(version, subPackets) {
+            override fun value(): Long = if (subPackets[0].value() > subPackets[1].value()) 1 else 0
+        }
+
+        class LessThan(version: Int, subPackets: List<BitPacket>) :
+            Operator(version, subPackets) {
+            override fun value(): Long = if (subPackets[0].value() < subPackets[1].value()) 1 else 0
+        }
+
+        class EqualTo(version: Int, subPackets: List<BitPacket>) :
+            Operator(version, subPackets) {
+            override fun value(): Long = if (subPackets[0].value() == subPackets[1].value()) 1 else 0
+        }
+
+        companion object {
+            fun of(version: Int, typeId: Int, subPackets: List<BitPacket>): Operator {
+                return when (typeId) {
+                    0 -> Sum(version, subPackets)
+                    1 -> Product(version, subPackets)
+                    2 -> Min(version, subPackets)
+                    3 -> Max(version, subPackets)
+                    5 -> GreaterThan(version, subPackets)
+                    6 -> LessThan(version, subPackets)
+                    7 -> EqualTo(version, subPackets)
+                    else -> throw IllegalArgumentException("Invalid type id: $typeId")
+                }
+            }
+        }
+    }
 
     companion object {
-        fun parse(bits: String, position: Int = 0, offset: Int = 0): Pair<BitPacket?, Int> {
-            if (bits.length < position + 11) { //need at least 11 bits for a packet
-                return null to position
+        fun parse(bits: String, start: Int = 0, upTo: Int = bits.length): Pair<BitPacket?, Int> {
+            if (upTo < start + 11) { //need at least 11 bits for a packet
+                return null to start
             }
-            val version = bits.substring(position..position + 2).toInt(2)
-            val typeId = bits.substring(position + 3..position + 5).toInt(2)
+            val version = bits.substring(start..start + 2).toInt(2)
+            val typeId = bits.substring(start + 3..start + 5).toInt(2)
             if (typeId == 4) { // literal
-                var nextGroupPosition = position + 6
+                var nextGroupPosition = start + 6
                 val literalBits = buildString {
                     do {
                         val hasMoreGroups = bits[nextGroupPosition] == '1'
@@ -21,34 +75,37 @@ sealed class BitPacket(val version: Int, val typeId: Int, val position: Int) {
                         nextGroupPosition += 5
                     } while (hasMoreGroups)
                 }
-                return Literal(version, typeId, position + offset, literalBits) to nextGroupPosition
+                return Literal(version, literalBits.toLong(2)) to nextGroupPosition
             } else {
-                val lengthType = bits[position + 6].digitToInt(2)
+                val lengthType = bits[start + 6].digitToInt(2)
                 if (lengthType == 0) {
-                    val subPacketsLength = bits.substring(position + 7..position + 21).toInt(2)
-                    val subPacketRepresentation = bits.substring(position + 22..position + 21 + subPacketsLength)
+                    val subPacketsLength = bits.substring(start + 7..start + 21).toInt(2)
                     val subPackets = buildList {
-                        var nextPosition = 0
+                        var nextPosition = start + 22
                         do {
-                            val (packet, pos) = parse(subPacketRepresentation, nextPosition, offset + 22)
+                            val (packet, pos) = parse(bits, nextPosition, start + 22 + subPacketsLength)
                             nextPosition = pos
                             packet?.let {
                                 add(it)
                             }
                         } while (packet != null)
                     }
-                    return Operator(version, typeId, position + offset, subPackets) to position + 22 + subPacketsLength
+                    return Operator.of(
+                        version,
+                        typeId,
+                        subPackets
+                    ) to start + 22 + subPacketsLength
                 } else {
-                    val subPacketCount = bits.substring(position + 7..position + 17).toInt(2)
-                    var next = position + 18
+                    val subPacketCount = bits.substring(start + 7..start + 17).toInt(2)
+                    var next = start + 18
                     val subPackets = buildList(subPacketCount) {
                         repeat(subPacketCount) {
-                            val (packet, pos) = parse(bits, next, offset)
+                            val (packet, pos) = parse(bits, next, upTo)
                             add(packet ?: throw IllegalStateException("Could not read all subpackets"))
                             next = pos
                         }
                     }
-                    return Operator(version, typeId, position + offset, subPackets) to next
+                    return Operator.of(version, typeId, subPackets) to next
                 }
             }
         }
@@ -74,8 +131,8 @@ fun main() {
         return score(packet)
     }
 
-    fun part2(input: String): Int {
-        TODO()
+    fun part2(input: String): Long {
+        return BitPacket.parse(hexToBinary(input)).first!!.value()
     }
 
     check(part1("8A004A801A8002F478") == 16)
@@ -86,6 +143,13 @@ fun main() {
     val input = readInput("Day16").first()
     println(part1(input))
 
-    check(part2(TODO()) == TODO())
+    check(part2("C200B40A82") == 3L)
+    check(part2("04005AC33890") == 54L)
+    check(part2("880086C3E88112") == 7L)
+    check(part2("CE00C43D881120") == 9L)
+    check(part2("D8005AC2A8F0") == 1L)
+    check(part2("F600BC2D8F") == 0L)
+    check(part2("9C005AC2F8F0") == 0L)
+    check(part2("9C0141080250320F1802104A08") == 1L)
     println(part2(input))
 }
