f4f4d6320474796ca7155efaa25198bb49479584
Bali Miklós <miklos.bali@dbx.hu>
2021-12-21 10:48:07 +0100

    Day 21 - simplify quantum game state
    we do not need to know which player rolls, if the state has the players in the order they will roll
    this results in a smaller state space and a smaller result cache

--- src/Day21.kt
+++ src/Day21.kt
@@ -37,59 +37,47 @@ object Day21 {
     fun String.toPlayer() = Player(position = Regex("""\d+$""").find(this)!!.value.toInt())
 
     data class QuantumGameState(
-        val player1Rolls: Boolean,
-        val player1Position: Int,
-        val player2Position: Int,
-        val player1Score: Int = 0,
-        val player2Score: Int = 0,
-        val winningScore: Int = 21,
-        val finished: Boolean = false
+        val rollingPlayerPosition: Int,
+        val otherPlayerPosition: Int,
+        val rollingPlayerScore: Int = 0,
+        val otherPlayerScore: Int = 0,
+        val winningScore: Int = 21
     ) {
+        fun finished() =
+            otherPlayerScore >= winningScore || rollingPlayerScore >= 21
+
         companion object {
-            val POSSIBLE_ROLLS = (1..3)
+            val POSSIBLE_ROLL_COUNTS = (1..3)
                 .flatMap { r1 ->
                     (1..3).flatMap { r2 ->
-                        (1..3).map { r3 -> Triple(r1, r2, r3) }
+                        (1..3).map { r3 -> r1 + r2 + r3 }
                     }
-                }
+                }.groupingBy { it }.eachCount().toList()
         }
     }
 
-    fun QuantumGameState.next(roll1: Int, roll2: Int, roll3: Int): QuantumGameState? {
-        if (finished) return null
-        else if (player1Rolls) {
-            val newPosition = (player1Position + roll1 + roll2 + roll3 - 1) % 10 + 1
-            return copy(
-                player1Rolls = false,
-                player1Position = newPosition,
-                player1Score = player1Score + newPosition,
-                finished = player1Score + newPosition >= winningScore
-            )
-        } else {
-            val newPosition = (player2Position + roll1 + roll2 + roll3 - 1) % 10 + 1
-            return copy(
-                player1Rolls = true,
-                player2Position = newPosition,
-                player2Score = player2Score + newPosition,
-                finished = player2Score + newPosition >= winningScore
-            )
+    fun QuantumGameState.next(roll: Int): QuantumGameState? =
+        if (finished()) null
+        else {
+            val newPosition = (rollingPlayerPosition + roll - 1) % 10 + 1
+            val newScore = rollingPlayerScore + newPosition
+            QuantumGameState(otherPlayerPosition, newPosition, otherPlayerScore, newScore)
         }
-    }
 
     fun QuantumGameState.part2(resultCache: MutableMap<QuantumGameState, Pair<Long, Long>> = mutableMapOf()): Pair<Long, Long> {
         if (this !in resultCache) {
             resultCache[this] = this.let { state ->
-                if (state.finished) {
-                    if (state.player1Score >= state.winningScore) {
+                if (state.finished()) {
+                    if (state.otherPlayerPosition >= state.winningScore) {
                         1L to 0L
                     } else {
                         0L to 1L
                     }
                 } else
-                    QuantumGameState.POSSIBLE_ROLLS.fold(0L to 0L) { acc, (r1, r2, r3) ->
-                        this.next(r1, r2, r3)!!.let { nextState ->
+                    QuantumGameState.POSSIBLE_ROLL_COUNTS.fold(0L to 0L) { acc, (roll, count) ->
+                        this.next(roll)!!.let { nextState ->
                             val p2 = nextState.part2(resultCache)
-                            acc.first + p2.first to acc.second + p2.second
+                            acc.first + p2.second * count to acc.second + p2.first * count
                         }
                     }
             }
@@ -113,7 +101,7 @@ fun main() {
 
     fun part2(input: List<String>): Long {
         val (player1, player2) = input.map { it.toPlayer() }
-        val wins = QuantumGameState(true, player1.position, player2.position).part2()
+        val wins = QuantumGameState(player1.position, player2.position).part2()
         return max(wins.first, wins.second)
     }
 
