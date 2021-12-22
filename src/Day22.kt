5218c8381076fb18f78037208dad93a57246e62e
Bali Miklós <miklos.bali@dbx.hu>
2021-12-22 11:57:39 +0100

    Day 22 - benchmarking
    * We can manipulate lists faster (but we must keep its elements unique)
    * When removing a region from another region then we can merge the resulting (at most 26) sub-regions into at most 10 sub-regions, keeping the reactor state smaller

--- src/Day22.kt
+++ src/Day22.kt
@@ -23,6 +23,54 @@ private object Day22 {
 
     private operator fun Region.contains(v: Vec3) = v.x in x && v.y in y && v.z in z
 
+    private infix fun IntRange.mergeableWith(other: IntRange): Boolean =
+        !intersect(other).isEmpty() ||
+                (last != Int.MAX_VALUE && other.last != Int.MAX_VALUE && (first == other.last + 1 || other.first == last + 1))
+
+    private infix fun IntRange.merge(other: IntRange): IntRange =
+        if (!this.mergeableWith(other)) {
+            throw IllegalArgumentException("Ranges are not mergeable")
+        } else {
+            val merged = min(first, other.first)..max(last, other.last)
+            assert(merged.size() == this.size() + other.size() - this.intersect(other).size()) {
+                "Unexpected merged int range size!"
+            }
+            merged
+        }
+
+    private infix fun Region.mergeableWith(other: Region): Boolean =
+        x.mergeableWith(other.x) &&
+                y.mergeableWith(other.y) &&
+                z.mergeableWith(other.z) &&
+                listOf(x, y, z).zip(listOf(other.x, other.y, other.z)).count { (a, b) -> a == b } >= 2
+
+    private infix fun Region.merge(other: Region): Region =
+        if (!this.mergeableWith(other)) {
+            throw IllegalArgumentException("Ranges are not mergeable")
+        } else {
+            val merged = Region(x merge other.x, y merge other.y, z merge other.z)
+            assert(
+                merged.size() == this.size() + other.size() - this.intersect(other).size()
+            ) { "Unexpected merged region size for $this and $other -> $merged" }
+            merged
+        }
+
+
+    private fun MutableCollection<Region>.addRegion(region: Region, mergeWithExistingRegions: Boolean = true) {
+        if (!mergeWithExistingRegions)
+            add(region)
+        else {
+            var newRegion = region
+            do {
+                val mergeable = this.firstOrNull { it mergeableWith newRegion }?.also { other ->
+                    remove(other)
+                    newRegion = newRegion.merge(other)
+                }
+            } while (mergeable != null)
+            add(newRegion)
+        }
+    }
+
     private operator fun Region.minus(other: Region): Set<Region> {
         val intersection = this intersect other
         return if (intersection.isEmpty())
@@ -32,19 +80,18 @@ private object Day22 {
         else
             buildSet {
                 for ((xRange, yRange, zRange) in crossJoin(
-                    this@minus.x splitWith other.x,
-                    this@minus.y splitWith other.y,
-                    this@minus.z splitWith other.z
+                    this@minus.x splitWith intersection.x,
+                    this@minus.y splitWith intersection.y,
+                    this@minus.z splitWith intersection.z
                 ))
                     if (xRange.first !in intersection.x ||
                         yRange.first !in intersection.y ||
                         zRange.first !in intersection.z
                     ) {
-                        add(Region(xRange, yRange, zRange))
+                        addRegion(Region(xRange, yRange, zRange))
                     }
             }
     }
-
     private fun Region.isEmpty(): Boolean = x.isEmpty() || y.isEmpty() || z.isEmpty()
     private fun IntRange.size(): Long = if (isEmpty()) 0L else 1L + last - first //last-first can be over Int.MAX_VALUE
 
@@ -64,7 +111,7 @@ private object Day22 {
 
     private data class ReactorState(
         val reactorRegion: Region = MAX_REGION,
-        val onRegions: Set<Region> = emptySet()
+        val onRegions: List<Region> = emptyList()
     )
 
     private infix fun ReactorState.apply(instruction: Instruction): ReactorState {
@@ -73,36 +120,38 @@ private object Day22 {
             this
         else {
             val newRegions =
-                onRegions.flatMap { it - affectedRegion }.toMutableSet() //remove on state for the affected region
-            if (instruction.state) newRegions += affectedRegion //add it if needed
+                onRegions.flatMapTo(mutableListOf()) { it - affectedRegion } //remove on state for the affected region
+            if (instruction.state) newRegions.addRegion(affectedRegion, false) //add it if needed
             copy(
                 onRegions = newRegions
             )
         }
     }
 
-    fun solution(input: List<String>, reactorRegion: Region = MAX_REGION): Long {
+    fun solution(input: List<String>, reactorRegion: Region = MAX_REGION, quiet: Boolean = true): Long {
         return input
             .toInstructions()
             .fold(ReactorState(reactorRegion)) { state, instruction -> state.apply(instruction) }
             .onRegions
+            .apply { if (!quiet) println("Region count: ${this.count()}") }
             .sumOf { it.size() }
     }
 }
 
 fun main() {
+    val quiet = true //for some debugging
 
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>, quiet: Boolean = true): Long {
         val reactorRegion = Region(
             -50..50,
             -50..50,
             -50..50
         )
-        return solution(input, reactorRegion)
+        return solution(input, reactorRegion, quiet = quiet)
     }
 
-    fun part2(input: List<String>): Long {
-        return solution(input)
+    fun part2(input: List<String>, quiet: Boolean = true): Long {
+        return solution(input, quiet = quiet)
     }
 
     val testInput = readInput("Day22_test")
@@ -111,8 +160,14 @@ fun main() {
     check(part1(testInput2) == 474140L)
 
     val input = readInput("Day22")
-    println(part1(input))
+    println(part1(input, quiet))
 
     check(part2(testInput2) == 2758514936282235L)
-    println(part2(input))
+    println(part2(input, quiet))
+
+    for (round in 1..3) {
+        println("------------ Round $round!-------- FIGHT!")
+        benchmark("part1", 100) { part1(input) }
+        benchmark("part2", 100) { part2(input) }
+    }
 }
