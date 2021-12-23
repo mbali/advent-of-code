4053c729a9506040e97fa11c4b5f61cce0edb50a
Bali Miklós <miklos.bali@dbx.hu>
2021-12-23 18:42:40 +0100

    Day 23 - part 2
    Maybe next time I should read the problem more carefully.
    I got tripped by enabling movement inside the hallway, so the possible space state was much larger.
    
    Unluckily part 1 with the wrong implementation returned correct results both for the sample and the real input.
    
    Rewrote the whole solution with a new state representation.

--- src/Day23.kt
+++ src/Day23.kt
@@ -1,18 +1,12 @@
-import Day23.AmphipodType.*
-import Day23.EXPECTED_STATE
-import Day23.LocationType.HALLWAY
-import Day23.LocationType.ROOM
-import Day23.calculateDistanceFrom
-import Day23.parseState
-import java.util.*
+import Day23.solution
 import kotlin.math.absoluteValue
 
 object Day23 {
-    enum class AmphipodType(val movementCost: Int, val marker: Char) {
-        AMBER(1, 'A'),
-        BRONZE(10, 'B'),
-        COPPER(100, 'C'),
-        DESERT(1000, 'D');
+    enum class AmphipodType(val movementCost: Int, val doorIndex: Int) {
+        AMBER(1, 2),
+        BRONZE(10, 4),
+        COPPER(100, 6),
+        DESERT(1000, 8);
 
         companion object {
             fun of(char: Char): AmphipodType? =
@@ -27,107 +21,217 @@ object Day23 {
         }
     }
 
-    fun AmphipodType?.marker(): Char = this?.marker ?: '.'
+    data class Room(val expected: AmphipodType, val size: Int, val occupants: List<AmphipodType>)
 
-    enum class LocationType {
-        HALLWAY,
-        ROOM
+    fun AmphipodType.canMoveInto(room: Room) =
+        room.expected == this && room.occupants.size < room.size && room.occupants.none { it != this }
+
+    fun Room.stepsToEnterFromDoor() = size - occupants.size
+    fun Room.stepsToExitToDoor() = size - occupants.size + 1
+    fun Room.hallwayIndex() = expected.doorIndex
+    fun Room.top() = occupants.last()
+    fun Room.removeTop() = copy(
+        occupants = occupants.dropLast(1)
+    )
+
+    fun Room.attTop(type: AmphipodType) = copy(
+        occupants = occupants + type
+    )
+
+    data class Hallway(val positions: List<AmphipodType?> = List(11) { null })
+
+    fun Hallway.freePositionsAround(index: Int): Set<Int> {
+        var left = index - 1
+        while (left >= 0 && positions[left] == null) left--
+        var right = index + 1
+        while (right < positions.size && positions[right] == null) right++
+        return (left + 1 until right).toSet() - HALLWAY_DOOR_INDICES - index
     }
 
-    data class Location(val type: LocationType, val x: Int, val y: Int, val expectedAmphipodType: AmphipodType?)
+    data class Movement(val type: AmphipodType, val steps: Int)
 
-    val LOCATIONS = buildList {
-        addAll(listOf(1, 2, 4, 6, 8, 10, 11).map { Location(HALLWAY, it, 1, null) })
-        listOf(AMBER to 3, BRONZE to 5, COPPER to 7, DESERT to 9).forEach { (type, x) ->
-            (2..3).forEach { y -> add(Location(ROOM, x, y, type)) }
+    fun Movement.cost() = type.movementCost * steps
+    fun Hallway.update(index: Int, type: AmphipodType?): Hallway {
+        return Hallway(buildList(positions.size) {
+            addAll(positions)
+            set(index, type)
+        })
+    }
+
+    fun Room.possibleToHallwayMovements(hallway: Hallway): List<Triple<Room, Hallway, Movement>> {
+        return if (occupants.none { it != expected }) emptyList()
+        else buildList {
+            val type = top()
+            val hallwayIdx = hallwayIndex()
+            val stepsToExit = stepsToExitToDoor()
+            hallway.freePositionsAround(hallwayIdx).forEach { idx ->
+                add(
+                    Triple(
+                        removeTop(),
+                        hallway.update(idx, type),
+                        Movement(type, (idx - hallwayIdx).absoluteValue + stepsToExit)
+                    )
+                )
+            }
         }
     }
 
-    private fun Location.isNeighbour(other: Location, allLocations: List<Location>): Boolean {
-        return if (other == this) false
-        else if (this.type == HALLWAY && other.type == HALLWAY && y == other.y) {
-            allLocations.filter { it.type == HALLWAY }
-                .none { it.x in x + 1 until other.x || it.x in other.x + 1 until x }
-        } else {
-            (x - other.x).absoluteValue <= 1 && (y - other.y).absoluteValue <= 1
-        }
-    }
-
-    val NEIGHBOURS = LOCATIONS.associateWith { from ->
-        LOCATIONS.filter { to -> from.isNeighbour(to, LOCATIONS) }
-            .associateWith { (Vec3(it.x, it.y) manhattanDistance Vec3(from.x, from.y)) }
-    }
-
-    val EXPECTED_STATE = LOCATIONS.map { it.expectedAmphipodType.marker() }.joinToString("")
-
-    fun String.neigbouringStates(): Map<String, Int> {
-        assert(this.length == LOCATIONS.size)
-        val initial = LOCATIONS.zip(this.map { AmphipodType.of(it) }).toMap()
-        return buildMap {
-            initial.filterValues { it != null }.forEach { (from, type) ->
-                val movementCost = type!!.movementCost
-                NEIGHBOURS.getValue(from).filterKeys { to ->
-                    initial.getValue(to) == null
-                }.filterKeys { to ->
-                    to.type == HALLWAY || from.type == to.type || to.expectedAmphipodType == type && NEIGHBOURS.getValue(
-                        to
-                    ).keys.filter { it.type == ROOM && initial[it] != null }
-                        .all { it.expectedAmphipodType == initial[it] }
-                }.forEach { (toLocation, cost) ->
-                    val next = initial.toMutableMap()
-                    next[from] = null
-                    next[toLocation] = type
-                    put(LOCATIONS.map { next[it].marker() }.joinToString(""), cost * movementCost)
+    fun Room.possibleFromHallwayMovements(hallway: Hallway): List<Triple<Room, Hallway, Movement>> {
+        val type = expected
+        return if (!type.canMoveInto(this)) emptyList()
+        else buildList {
+            val hallwayIdx = hallwayIndex()
+            val stepsToEnter = stepsToEnterFromDoor()
+            for (i in hallwayIdx downTo 0) {
+                if (hallway.positions[i] == type) {
+                    add(
+                        Triple(
+                            attTop(type),
+                            hallway.update(i, null),
+                            Movement(type, (i - hallwayIdx).absoluteValue + stepsToEnter)
+                        )
+                    )
+                }
+                if (hallway.positions[i] != null) {
+                    break
+                }
+            }
+            for (i in hallwayIdx until hallway.positions.size) {
+                if (hallway.positions[i] == type) {
+                    add(
+                        Triple(
+                            attTop(type),
+                            hallway.update(i, null),
+                            Movement(type, (i - hallwayIdx).absoluteValue + stepsToEnter)
+                        )
+                    )
+                }
+                if (hallway.positions[i] != null) {
+                    break
                 }
             }
         }
     }
 
-    fun List<String>.parseState(): String {
-        return LOCATIONS.map { this[it.y][it.x] }.joinToString("")
+    val HALLWAY_DOOR_INDICES = AmphipodType.values().map { it.doorIndex }.toSet()
+
+    data class MapState(
+        val rooms: Set<Room>,
+        val hallway: Hallway
+    )
+
+    fun MapState.neighbours(): Map<MapState, Int> =
+        buildMap {
+            rooms.forEach { room ->
+                (room.possibleFromHallwayMovements(hallway) + room.possibleToHallwayMovements(hallway)).forEach { (newRoom, newHallway, movement) ->
+                    put(
+                        copy(
+                            rooms = rooms - room + newRoom,
+                            hallway = newHallway
+                        ),
+                        movement.cost()
+                    )
+                }
+            }
+        }
+
+    fun List<String>.parseMapState(roomSize: Int): MapState {
+        val hallway = this[1].substring(1..11).map { AmphipodType.of(it) }.let { Hallway(it) }
+        val rooms = AmphipodType.values().map { type ->
+            (1..roomSize)
+                .map { it + 1 }
+                .mapNotNull { AmphipodType.of(this[it][type.doorIndex + 1]) }
+                .reversed()
+                .let { Room(type, roomSize, it) }
+        }.toSet()
+        return MapState(rooms, hallway)
     }
 
-    fun calculateDistanceFrom(start: String, target: String): Int {
-        val queue = PriorityQueue<Pair<String, Int>>(compareBy { it.second })
-        val seen = mutableSetOf<String>()
-        queue.offer(start to 0)
+    fun expectedMapState(roomSize: Int): MapState {
+        val hallway = Hallway()
+        val rooms = AmphipodType.values().map { type ->
+            Room(type, roomSize, List(roomSize) { type })
+        }.toSet()
+        return MapState(rooms, hallway)
+    }
+
+
+    fun MapState.calculateDistanceTo(target: MapState): Int {
+        val seen = mutableSetOf<MapState>()
+        val minDistancesByState = mutableMapOf<MapState, Int>().withDefault { Int.MAX_VALUE }
+        val statesByDistances = mutableMapOf<Int, MutableSet<MapState>>()
+
+        fun registerCandidate(state: MapState, distance: Int) {
+            if (state in seen) return
+            val currentMinDistance = minDistancesByState.getValue(state)
+            if (distance < currentMinDistance) {
+                statesByDistances[currentMinDistance]?.remove(state)
+                minDistancesByState[state] = distance
+                statesByDistances.computeIfAbsent(distance) {
+                    mutableSetOf()
+                }.add(state)
+            }
+        }
+
+        statesByDistances[0] = mutableSetOf(this)
         do {
-            val maybeNext = queue.poll()
-            maybeNext?.let { next ->
-                val nextState = next.first
-                if (!seen.contains(nextState)) {
-                    val distance = next.second
-                    if (nextState.contentEquals(target)) return distance
-                    seen.add(nextState)
-                    nextState.neigbouringStates().filter { !seen.contains(it.key) }.forEach { (state, cost) ->
-                        val candidateDistance = distance + cost
-                        queue.offer(state to candidateDistance)
-                    }
-                }
+            val distance = statesByDistances.keys.minOfOrNull { it } ?: SHOULD_NOT_REACH()
+            val statesForDistance = statesByDistances.remove(distance)!!
+            if (target in statesForDistance) {
+                return distance
             }
-        } while (maybeNext != null)
+            statesForDistance
+                .mapNotNull { nextState ->
+                    if (nextState in seen) null
+                    else nextState to nextState.neighbours()
+                }
+                .forEach { (state, neighbours) ->
+                    seen.add(state)
+                    neighbours
+                        .forEach { (state, cost) ->
+                            registerCandidate(state, cost + distance)
+                        }
+                }
+        } while (statesByDistances.isNotEmpty())
         SHOULD_NOT_REACH()
     }
+
+
+    fun solution(input: List<String>): Int {
+        val roomSize = input.size - 3
+        val initialState = input.parseMapState(roomSize)
+        val expectedState = expectedMapState(roomSize)
+        return initialState.calculateDistanceTo(expectedState)
+    }
+
 }
 
 fun main() {
 
     fun part1(input: List<String>): Int {
-        val initialState = input.parseState()
-        return calculateDistanceFrom(initialState, EXPECTED_STATE)
+        val foldedInput = input.toMutableList()
+        foldedInput.removeAt(4)
+        foldedInput.removeAt(3)
+        return solution(foldedInput)
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        return solution(input)
     }
 
     val testInput = readInput("Day23_test")
-    check(part1(testInput) == 12521)
 
+    check(part1(testInput) == 12521)
     val input = readInput("Day23")
     println(part1(input))
 
-    check(part2(testInput) == TODO())
+    check(part2(testInput) == 44169)
     println(part2(input))
+
+    for (round in 1..3) {
+        println("------------ Round $round!-------- FIGHT!")
+        benchmark("part1", 10) { part1(input) }
+        benchmark("part2", 10) { part2(input) }
+    }
 }
 
